6d04956073548c03c12c218a79cea36a2518d454
Ewan Lyon <clubwhomc@gmail.com>
2016-08-08 18:47:29 +1000

    Update Utilities.py

--- Utilities.py
+++ Utilities.py
@@ -1,33 +1,38 @@
+#HUGE Help from Caleb <3
+
 import goslate
 import microsofttranslator
 from microsofttranslator import Translator
+from discord.ext import commands
+
+# Setup translate commands
+gs = goslate.Goslate()
+translator = Translator('WomBot', 'Token')
+
 
 class Utilities():
     def __init__(self, bot):
         self.bot = bot
 
-        # Setup translate commands
-        gs = goslate.Goslate()
-        translator = Translator('WomBot', 'Token')
+    # Translates a language into english
+    @commands.command(pass_context=True)
+    async def translate(self, ctx):
+        """Translates something into english"""
 
-        # Translates a language into english
-        @bot.command(pass_context=True)
-        async def translate(ctx):
-            """Translates something into english"""
-
-            message = ctx.message
-            ctx = ctx.message.content[len(ctx.message.content.split(" ")[0]) + 1:]
-            print(message.author.name + "@" + message.server.name + "~" + message.channel.name + ": " + message.content)
-            try:
-                curlanguage = translator.detect_language(ctx)
-                translated = translator.translate(ctx, 'en')
-                curlanguagefull = gs.get_languages()[curlanguage]
-                await bot.say('Translated from **' + curlanguagefull + '**: ' + translated)
-            except microsofttranslator.TranslateApiException:
-                await bot.say("Timed out from server - please try again in a few seconds.")
-            except microsofttranslator.ArgumentOutOfRangeException:
-                await bot.say("Invalid language.")
+        message = ctx.message
+        ctx = ctx.message.content[len(ctx.message.content.split(" ")[0]) + 1:]
+        print(message.author.name + "@" + message.server.name + "~" + message.channel.name + ": " + message.content)
+        try:
+            curlanguage = translator.detect_language(ctx)
+            translated = translator.translate(ctx, 'en')
+            curlanguagefull = gs.get_languages()[curlanguage]
+            await self.bot.say('Translated from **{}**: {}'.format(curlanguagefull, translated))
+        except microsofttranslator.TranslateApiException:
+            await self.bot.say("Timed out from server - please try again in a few seconds.")
+        except microsofttranslator.ArgumentOutOfRangeException:
+            await self.bot.say("Invalid language.")
 
 
 def setup(bot):
     bot.add_cog(Utilities(bot))
+
