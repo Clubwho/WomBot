8d7dd87975c7351008f59d62895e1ab182159d8f
Ewan Lyon <clubwhomc@gmail.com>
2016-08-12 07:25:36 +1000

    Update Memes.py

--- Memes.py
+++ Memes.py
@@ -1,35 +1,14 @@
-#HUGE Help from Caleb <3
+# HUGE Help from Caleb <3
 
-import sys
+import discord
 import json
 import random
+import re
 from discord.ext import commands
 
-#Videos for bork command
-borkoptions = ["https://www.youtube.com/watch?v=vwGnXKNGjT0", "https://www.youtube.com/watch?v=5i2brBA_dl8",
-               "https://www.youtube.com/watch?v=b2p8Zxmuq4g", "https://www.youtube.com/watch?v=AeEH5ugJrUU",
-               "https://www.youtube.com/watch?v=i1H0leZhXcY", "https://www.youtube.com/watch?v=gIx6_Srsrog",
-               "https://www.youtube.com/watch?v=5dbG4wqN0rQ"]
+with open('Lists.json') as json_data:
+    jsonlist = json.load(json_data)
 
-#Meme help table
-memehelp = """```Meme help
-
-Memes:
-  yee         Posts yee
-  wow         Posts wow gif
-  wew         See wow
-  lenny       ( ͡° ͜ʖ ͡° )
-  keem        Keemstar dancing
-  straya      Go Aussie Go
-  attackheliI sexually identify as an Attack Helicopter
-  song        Hey baby
-  wtf finland Loituma - "Ievan Polkka"
-  tunak       TUNAK TUNAK TUNAKTUNAK TUNAK
-  help        Posts this```
-"""
-
-with open('MemeList.json') as json_data:
-    memelist = json.load(json_data)
 
 class Memes():
     def __init__(self, bot):
@@ -41,59 +20,86 @@ class Memes():
         """Posts a meme, Use &meme help to see them all"""
 
         message = ctx.message
-        ctx = ctx.message.content[len(ctx.message.content.split(" ")[0]) + 1:]
+        ctx = ctx.message.content.split(' ', 2)[1]
         print(message.author.name + "@" + message.server.name + "~" + message.channel.name + ": " + message.content)
-        try:
-            await self.bot.say(memelist['MemeList'][ctx])
-        except:
-            await self.bot.say("That isn't a meme I know! **Use &meme help** to see all the memes I can say.")
+        if ctx == 'help':
+            memelistsort = sorted(jsonlist['MemeList'].keys())
+            memeliststring = str(memelistsort)
+            memehelp = re.sub("[\[\]']", "", memeliststring)
+            await self.bot.say("I know: **{}**.".format(memehelp))
+        elif ctx =='nummemes':
+            await self.bot.say('I know **{}** memes!'.format(len(jsonlist['MemeList'])))
+        else:
+            try:
+                await self.bot.say(jsonlist['MemeList'][ctx])
+            except:
+                await self.bot.say("That isn't a meme I know! **Use &meme help** to see all the memes I can say.")
 
-    # Posts a random bork video from borkoptions
-    @commands.command()
-    async def bork(self):
-        """Posts a random bork video"""
-
-        randbork = (random.choice(borkoptions))
-        await self.bot.say(randbork)
-
-    #Meme testing
+    # Meme testing
     @commands.command(pass_context=True, hidden=True)
-    async def addmeme(self, ctx, memename : str, memeurl : str):
+    async def addmeme(self, ctx, memename: str, memeurl: str):
         message = ctx.message
-        ctx = ctx.message.content[len(ctx.message.content.split(" ")[0]) + 1:]
+        ctx = ctx.message.content.split(' ', 2)[1]
         print(message.author.name + "@" + message.server.name + "~" + message.channel.name + ": " + message.content)
         try:
             if message.author.id == '132111332752359424':
-                if memename in memelist['MemeList']:
-                    await self.bot.say('**{}** already exists!'.format(memename))
+                if memename in jsonlist['MemeList']:
+                    await self.bot.say('**{}** already exists!'.format(ctx))
                 else:
-                    memelist['MemeList'][memename] = memeurl
-                    with open('MemeList.json', 'w') as a:
-                        a.write(json.dumps(memelist))
+                    jsonlist['MemeList'][memename] = memeurl
+                    with open('Lists.json', 'w') as a:
+                        a.write(json.dumps(jsonlist, indent=4))
                     await self.bot.say('Added **{}**'.format(memename))
             else:
-                await self.bot.say('Only @Clubwho can add memes')
+                await self.bot.say('Only Clubwho can add memes')#.format(discord.Object(132111332752359424).mention)
         except Exception as e:
-            print (e)
+            print(e)
 
     @commands.command(pass_context=True, hidden=True)
-    async def removememe(self, ctx, memename : str):
+    async def removememe(self, ctx, memename: str):
         message = ctx.message
-        ctx = ctx.message.content[len(ctx.message.content.split(" ")[0]) + 1:]
+        ctx = ctx.message.content.split(' ', 2)[1]
         print(message.author.name + "@" + message.server.name + "~" + message.channel.name + ": " + message.content)
         try:
             if message.author.id == '132111332752359424':
-                if memename in memelist['MemeList']:
-                    del memelist['MemeList'][memename]
-                    with open('MemeList.json', 'w') as a:
-                        a.write(json.dumps(memelist))
+                if memename in jsonlist['MemeList']:
+                    del jsonlist['MemeList'][memename]
+                    with open('Lists.json', 'w') as a:
+                        a.write(json.dumps(jsonlist, indent=4))
                     await self.bot.say('Removed **{}**'.format(memename))
                 else:
                     await self.bot.say('**{}** isn\'t a meme'.format(memename))
             else:
-                await self.bot.say('Only @Clubwho can remove memes')
+                await self.bot.say('Only Clubwho can remove memes')
         except:
             return
+
+    # Posts a random bork video
+    @commands.group(pass_context=True)
+    async def bork(self, ctx):
+        """Posts a random bork video"""
+        if ctx.invoked_subcommand is None:
+            randbork = (random.choice(jsonlist['BorkList']))
+            await self.bot.say(randbork)
+
+    @bork.command(pass_context=True)
+    async def add(self, ctx):
+        message = ctx.message
+        ctx = ctx.message.content.split(' ', 2)[2]
+        if message.author.id == '132111332752359424':
+            if ctx in jsonlist['BorkList']:
+                await self.bot.say('Tha\'s already been borked!')
+            else:
+                jsonlist['BorkList'].append(ctx)
+                with open('Lists.json', 'w') as a:
+                    a.write(json.dumps(jsonlist, indent=4))
+                await self.bot.say('**bork** *bork* `bork` bork ***BORK***')
+        else:
+            await self.bot.say('Only Clubwho can add borks')
+
+    @bork.command()
+    async def num(self):
+        await self.bot.say('There are **{}** borks!'.format(len(jsonlist['BorkList'])))
 
 def setup(bot):
     bot.add_cog(Memes(bot))
